9c35533a905069e74af3e007b8936ede50ad84a3
jozue06 <jozue06@gmail.com>
2022-02-11 22:26:33 -0600

    n

--- app.py
+++ app.py
@@ -5,24 +5,19 @@ import json
 app = Flask(__name__, static_url_path='', static_folder='app/build')
 
 
-@app.route("/", methods=["GET"])
-def serve():
+@app.route("/react", methods=["GET"])
+def indexReact():
     return send_from_directory(app.static_folder, 'index.html')
 
-# @app.route("/", methods=["GET"])
-# def indexHtml():
-# 	return render_template("index.html")
+@app.route("/", methods=["GET"])
+def indexHtml():
+	return render_template("index.html")
 
 @app.route("/api/html/search", methods=["POST"])
 def searchHtml():
 	queryString = request.form.get("queryString")
 	results = processIntoHtml(queryString)
 	return render_template("index.html", results=results)
-
-
-@app.route("/api/json", methods=["GET"])
-def index():
-	return render_template("index.html")
 
 @app.route("/api/json/search", methods=["POST"])
 def searchJson():
