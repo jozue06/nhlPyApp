0cebfa6dfa027f8b43f8815b17a40807a9fc6470
jozue06 <jozue06@gmail.com>
2022-02-11 22:10:19 -0600

    add react

--- app.py
+++ app.py
@@ -1,18 +1,35 @@
-from parser import processIntoHtml
-from flask import Flask, render_template, request, Response
+from parser import processIntoHtml, processIntoJSON
+from flask import Flask, render_template, send_from_directory, request, Response
 from flask_cors import CORS
-app = Flask(__name__)
+import json
+app = Flask(__name__, static_url_path='', static_folder='app/build')
 CORS(app)
 
-@app.route("/")
+@app.route("/react")
+def serve():
+    return send_from_directory(app.static_folder, 'index.html')
+
+@app.route("/", methods=["GET"])
+def indexHtml():
+	return render_template("index.html")
+
+@app.route("/api/html/search", methods=["POST"])
+def searchHtml():
+	queryString = request.form.get("queryString")
+	results = processIntoHtml(queryString)
+	return render_template("index.html", results=results)
+
+
+@app.route("/api/json", methods=["GET"])
 def index():
 	return render_template("index.html")
 
-@app.route("/search", methods=["POST"])
-def search():
+@app.route("/api/json/search", methods=["POST"])
+def searchJson():
 	queryString = request.get_json(force=True).get("queryString")
-	results = processIntoHtml(queryString)
-	return Response(results)
+	results = processIntoJSON(queryString)
+
+	return Response(json.dumps(results))
 
 if __name__ == '__main__':
     app.run()
