d3d408fd2620ad4db61caf4a477feaf0a14d3f5b
jozue06 <jozue06@gmail.com>
2022-02-11 22:19:49 -0600

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import json
 app = Flask(__name__, static_url_path='', static_folder='app/build')
 
 
-@app.route("/react")
+@app.route("/react", methods=["GET"])
 def serve():
     return send_from_directory(app.static_folder, 'index.html')
 
